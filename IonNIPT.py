c97b9ea0d2faab696475f715ee789dd9a28d6c9f
Alexandre Cormier <cormieralexandre@gmail.com>
2016-09-09 11:16:21 +0200

    first commit

--- IonNIPT.py
+++ IonNIPT.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 from ion.plugin import * 
 import os
-#import sys
-#import json 
 import subprocess 
 from joblib import Parallel, delayed
-#import glob 
-#import pickle
+import pickle
 from django.template import Context, Template
 from django.conf import settings
 
@@ -18,15 +15,21 @@ class IonNIPT(IonPlugin):
 	envDict = dict(os.environ)
 	
 	def launch(self, data=None):
-		print("Launch started...")
+		print "Launch started..."
 		
 		# ================ GET GLOBAL PATH
-		self.outputDir 		= os.environ["RESULTS_DIR"];  # The wisecondor results directory
+		self.outputDir 		= os.environ["RESULTS_DIR"];  # The plugin results directory
 		self.analysisDir 	= os.environ["ANALYSIS_DIR"];
 		self.pluginDir		= os.environ["PLUGIN_PATH"];
 		self.urlRoot 		= os.environ["URL_ROOT"]   # /output/Home/X/
 		self.urlPlugin 		= os.environ["TSP_URLPATH_PLUGIN_DIR"] # /output/Home/X/plugin_out/IonWisecondor
 		self.date               = os.environ["TSP_ANALYSIS_DATE"]
+		
+		self.retro = os.path.join(self.pluginDir, 'sanefalcon/retro.py')
+		self.getProfile = os.path.join(self.pluginDir, 'sanefalcon/getProfile.py')
+		self.predict = os.path.join(self.pluginDir, 'sanefalcon/predict.sh')
+		self.nuclTrack = os.path.join(self.pluginDir, 'data')
+		self.trainModel = os.path.join(self.pluginDir, 'data/trainModel-brest.model')
 		
 		# ================ GET INSTANCE PARAMETERS AND STORE THEM IN A LIST 
 		fileCount = int(os.environ["PLUGINCONFIG__COUNT"])
@@ -52,18 +55,24 @@ class IonNIPT(IonPlugin):
 			
 		# ================ LOOP ON EACH FILES AND START COMPUTATION 
 		for item in files:
+			print "Analyse {sample}".format(sample=item["sample"])
+			print "*** Detect trisomy using Wisecondor ***"
+			print "Get pickle"
 			# pickle
 			cmd_pickle = "samtools view {bam} -q 1 | python {pluginDir}/wisecondor/consam.py -outfile {outputDir}/{sample}_{date}.pickle".format(bam=item["input"], pluginDir = self.pluginDir, outputDir = self.outputDir, sample = item["sample"], date= self.date)
 			self.jobLauncher(cmd_pickle)
 			
+			print "Make GC correction"
 			# gcc
 			cmd_gcc = "python {pluginDir}/wisecondor/gcc.py  {outputDir}/{sample}_{date}.pickle {pluginDir}/data/hg19.gccount {outputDir}/{sample}_{date}.gcc".format(pluginDir = self.pluginDir, outputDir = self.outputDir, sample = item["sample"], date= self.date)
 			self.jobLauncher(cmd_gcc)
 			
+			print "Test"
 			# tested
 			cmd_tested = "python {pluginDir}/wisecondor/test.py {outputDir}/{sample}_{date}.gcc {pluginDir}/data/reftable {outputDir}/{sample}_{date}.tested".format(pluginDir = self.pluginDir, outputDir = self.outputDir, sample = item["sample"], date= self.date)
 			self.jobLauncher(cmd_tested)
 			
+			print "Generate the pdf file"
 			# pdf
 			cmd_pdf = "python {pluginDir}/wisecondor/plot.py {outputDir}/{sample}_{date}.tested  {outputDir}/{sample}_{date}".format(pluginDir = self.pluginDir, outputDir = self.outputDir, sample = item["sample"], date= self.date)	
 			self.jobLauncher(cmd_pdf)
@@ -75,18 +84,30 @@ class IonNIPT(IonPlugin):
 			item["s13"] = self.scoreOf(filePath, "13")
 			
 			### Saneflacon
-			# prep folder
-			# os.makedirs(os.path.join(self.outputDir, item["input"].split('.')[0]))
-			# 
-			# # readstarts
-			# Parallel(n_jobs=16)(delayed(getReadStarts)(item["input"], id) for id in range(1,23))
+			print "*** Get foetal fraction using Sanefalcon ***"
 			
-			# # nucProfile
-			# Parallel(n_jobs=16)(delayed(getProfiles)(item["input"].split('.')[0]), self.outputDir) for sample in os.listdir(os.path.join(self.outputDir, item["sample"])))
-			# 
-			# # test FF
-			# cmd_ff = "predict.sh {model} nucProfile/{sample}"
-			# self.jobLauncher(cmd_ff)
+			# prep folder
+			os.makedirs(os.path.join(self.outputDir, item["barcode"]))
+			barcode_folder = os.path.join(self.outputDir, item["barcode"])
+			
+			# readstarts
+			print "Compute readstarts"
+			Parallel(n_jobs=16)(delayed(getReadStarts)(item["input"], id, barcode_folder, self.retro) for id in range(1,23))
+			
+			# nucProfile
+			print "Get nucleosome profile"
+			Parallel(n_jobs=16)(delayed(getProfiles)(os.path.join(barcode_folder, readstarts), self.getProfile, self.nuclTrack, barcode_folder) for readstarts in os.listdir(barcode_folder))
+			
+			# test FF
+			print "Compute the foetal fraction"
+			cmd_ff = "bash {predict} {trainModel} {barcode_folder}/{barcode}".format(predict=self.predict, trainModel=self.trainModel, barcode_folder=barcode_folder, barcode=item["barcode"])
+			self.jobLauncher(cmd_ff)
+			
+			ff = open(os.path.join(barcode_folder, item["barcode"])+'.ff','r')
+			for line in ff:
+				elem = line.split()
+				if elem[0] == 'Fetal':
+					item["ff"] = round(float(elem[2]), 2)
 		
 		# ================ GENERATE RESULTS HTML FROM DJANGO TEMPLATE SYSTEM
 		settings.configure()
@@ -119,50 +140,69 @@ class IonNIPT(IonPlugin):
 		else:
 			raise Exception(stderr)
 	
-	# def getReadStarts(self, bam, id):
-	# 	
-	# 	retro = self.pluginDir + '/sanefalcon/retro.py'
-	# 	name, extension = bam.split('.')
-	# 	
-	# 	cmd_forward = "samtools view {bam} chr{id} -F 20 -q 1 | python {retro} | awk ".format(bam=bam, retro=retro, id=id)
-	# 	cmd_forward += "'{print $4}' > "
-	# 	cmd_forward += "{name}/{name}.{id}.start.fwd".format(name=name, id=id)
-	# 	
-	# 	cmd_reverse = "samtools view {bam} chr{id} -f 16 -F 4 -q 1 | python {retro} | awk ".format(bam=bam, retro=retro, id=id)
-	# 	cmd_reverse += "'{print ($4 + length($10) - 1)}' "
-	# 	cmd_reverse += "> {name}/{name}.{id}.start.rev".format(name=name, id=id)
-	# 	
-	# 	process = subprocess.Popen(cmd_forward, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	# 	process.communicate()
-	# 	process = subprocess.Popen(cmd_reverse, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	# 	process.communicate()
-	# 	
-	# def getProfiles(self, sample, dataDir):
-	# 
-	# 	nuclTrak = '/home/acormier/working_directory/DPNI/data/roy-nucleosome-track'
-	# 	getProfile = self.pluginDir + '/getProfile.py'
-	# 	
-	# 	name, chr, type, strand = sample.split('.')
-	# 	
-	# 	if strand == 'fwd':
-	# 		cmd_0 = "python {getProfile} {nuclTrak}/nucl_exR.{chr} {name}/{name}.{chr}.start.fwd 0 {name}/{name}.{chr}.fwd".format(getProfile=getProfile, nuclTrak=nuclTrak, input=input, chr=chr, sample=sample, name=name, output=output)
-	# 		cmd_1 = "python {getProfile} {nuclTrak}/nucl_exR.{chr} {name}/{name}.{chr}.start.fwd 1 {name}/{name}.{chr}.ifwd".format(getProfile=getProfile, nuclTrak=nuclTrak, input=input, chr=chr, sample=sample, name=name, output=output)
-	# 		
-	# 		process = subprocess.Popen(cmd_0, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	# 		process.communicate()
-	# 		
-	# 		process = subprocess.Popen(cmd_1, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	# 		process.communicate()
-	# 		
-	# 	elif strand == 'rev':
-	# 	       cmd_0 = "python {getProfile} {nuclTrak}/nucl_exR.{chr} {name}/{name}.{chr}.start.rev 1 {name}/{name}.{chr}.rev".format(getProfile=getProfile, nuclTrak=nuclTrak, input=input, chr=chr, sample=sample, name=name, output=output)
-	# 	       cmd_1 = "python {getProfile} {nuclTrak}/nucl_exR.{chr} {name}/{name}.{chr}.start.rev 0 {name}/{name}.{chr}.irev".format(getProfile=getProfile, nuclTrak=nuclTrak, input=input, chr=chr, sample=sample, name=name, output=output)
-	# 	       
-	# 	       process = subprocess.Popen(cmd_0, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	# 	       process.communicate()
-	# 	       
-	# 	       process = subprocess.Popen(cmd_1, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	# 	       process.communicate()
+def getReadStarts(bam, id, output, retro): #remove from class, joblib error
+	name = os.path.basename(bam).split('.')[0][:-7]
+	
+	cmd_forward = "samtools view {bam} chr{id} -F 20 -q 1 | python {retro} | awk ".format(bam=bam, retro=retro, id=id)
+	cmd_forward += "'{print $4}' > "
+	cmd_forward += "{output}/{name}.{id}.start.fwd".format(output=output, name=name, id=id)
+	
+	cmd_reverse = "samtools view {bam} chr{id} -f 16 -F 4 -q 1 | python {retro} | awk ".format(bam=bam, retro=retro, id=id)
+	cmd_reverse += "'{print ($4 + length($10) - 1)}' "
+	cmd_reverse += "> {output}/{name}.{id}.start.rev".format(output=output, name=name, id=id)
+	
+	process = subprocess.Popen(cmd_forward, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	stdout, stderr = process.communicate()
+	if process.returncode == 0:
+		pass
+	else:
+		raise Exception(stderr)
+	
+	process = subprocess.Popen(cmd_reverse, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	stdout, stderr = process.communicate()
+	if process.returncode == 0:
+		pass
+	else:
+		raise Exception(stderr)
+		
+def getProfiles(readstarts, getProfile, nuclTrak, barcode_folder):
+	name, chr, type, strand = os.path.basename(readstarts).split('.')
+	
+	if strand == 'fwd':
+		cmd_0 = "python {getProfile} {nuclTrak}/nuclTrack.{chr} {barcode_folder}/{name}.{chr}.start.fwd 0 {barcode_folder}/{name}.{chr}.fwd".format(getProfile=getProfile, nuclTrak=nuclTrak, chr=chr, name=name, barcode_folder=barcode_folder)
+		cmd_1 = "python {getProfile} {nuclTrak}/nuclTrack.{chr} {barcode_folder}/{name}.{chr}.start.fwd 1 {barcode_folder}/{name}.{chr}.ifwd".format(getProfile=getProfile, nuclTrak=nuclTrak, chr=chr, name=name, barcode_folder=barcode_folder)
+		
+		process = subprocess.Popen(cmd_0, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		stdout, stderr = process.communicate()
+		if process.returncode == 0:
+			pass
+		else:
+			raise Exception(stderr)
+		
+		process = subprocess.Popen(cmd_1, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		stdout, stderr = process.communicate()
+		if process.returncode == 0:
+			pass
+		else:
+			raise Exception(stderr)
+		
+	elif strand == 'rev':
+		cmd_0 = "python {getProfile} {nuclTrak}/nuclTrack.{chr} {barcode_folder}/{name}.{chr}.start.rev 1 {barcode_folder}/{name}.{chr}.rev".format(getProfile=getProfile, nuclTrak=nuclTrak, chr=chr, name=name, barcode_folder=barcode_folder)
+		cmd_1 = "python {getProfile} {nuclTrak}/nuclTrack.{chr} {barcode_folder}/{name}.{chr}.start.rev 0 {barcode_folder}/{name}.{chr}.irev".format(getProfile=getProfile, nuclTrak=nuclTrak, chr=chr, name=name, barcode_folder=barcode_folder)
+		
+		process = subprocess.Popen(cmd_0, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		stdout, stderr = process.communicate()
+		if process.returncode == 0:
+			pass
+		else:
+			raise Exception(stderr)
+		
+		process = subprocess.Popen(cmd_1, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		stdout, stderr = process.communicate()
+		if process.returncode == 0:
+			pass
+		else:
+			raise Exception(stderr)
 	
 if __name__ == "__main__":
   PluginCLI()
