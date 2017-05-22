092451dff30d992e25edc4e4efe3405c3e3881e0
AllaSSX <cormieralexandre@gmail.com>
2017-05-22 15:40:46 +0200

    Synthax correction + add messages

--- utils/IonNIPT_cluster.py
+++ utils/IonNIPT_cluster.py
@@ -23,53 +23,64 @@ def getArgs():
 def main(args):
 
 	### get BAM files (allows parallelisation)
+	print('Get BAM list...')
 	bam_lst = []
 	for bam in os.listdir(args.bamDir):
 		if bam.endswith(".bam"):
 			bam_lst.append(bam)
-			
+	print('Done')
+	
 	### Make Wisecondor analysis (in //)
-	Parallel(n_jobs=80)(delayed(wisecondor)(bam, args.bamDir, args.outDir, args.pluginDir) for bam in bam_lst)
+	print('WiseCondor analysis running...')
+	Parallel(n_jobs=16)(delayed(wisecondor)(bam, args.bamDir, args.outDir, args.pluginDir) for bam in bam_lst)
+	print('Done')
 	
 	### Make Sanefalcon analysis
-
+	
+	print('Start Sanefalcon analysis...')
 	readstarts = os.path.join(args.outDir, 'readstarts')
 	os.makedirs(readstarts)
 	
 	## Get readstarts for each bam, in // per chromosome
+	print('First step, get readstarts')
 	for bam in bam_lst:	
-		Parallel(n_jobs=23)(delayed(getReadStarts)(bam, id, args.bamDir, readstarts, args.pluginDir) for id in range(1,23))
+		Parallel(n_jobs=16)(delayed(getReadStarts)(bam, id, args.bamDir, readstarts, args.pluginDir) for id in range(1,23))
+	print('Readstarts: OK')
 	
 	## Get nucleosome profiles
-	profiles = = os.path.join(args.outDir, 'profiles')
+	profiles = os.path.join(args.outDir, 'profiles')
 	os.makedirs(profiles)
 	
-	Parallel(n_jobs=80)(delayed(getProfiles)(readstart, args.pluginDir, readstarts, profiles) for readstart in os.listdir(readstarts))
-			
+	print('Second step, get nucleosomes profiles')
+	Parallel(n_jobs=16)(delayed(getProfiles)(readstart, args.pluginDir, readstarts, profiles) for readstart in os.listdir(readstarts))
+	print('Profiles: OK')	
 	## Get foetal fraction
 	
 	predict = os.path.join(args.pluginDir, 'sanefalcon/predict.sh')
 	trainModel = os.path.join(args.pluginDir, 'data/trainModel_BorBreCoc_defrag-rassf1.model')
-	
-	ff_sanefalcon
+	print('Final step, get ff with Sanefalcon')
+	ff_sanefalcon = {}
 	for bam in bam_lst:
 		name = os.path.basename(bam).split('.')[0]
-		bam = os.path.join(profiles, bam)
+		bam = os.path.join(profiles, name)
 		
 		ff_cmd = "bash {predict} {trainModel} {bam}".format(predict=predict, trainModel=trainModel, bam=bam)
 		jobLauncher(ff_cmd)
 		
-		ff = open(os.path.join(bam + '.ff','r'))
+		ff = open(os.path.join(bam + '.ff'),'r')
 		for line in ff:
 			elem = line.split()
 			if elem[0] == 'Fetal':
 				ff_sanefalcon[name] = round(float(elem[2]), 2)
+	
+	for key, value in sorted(ff_sanefalcon.items()):
+		print key, value
 		
+	print('Done')
+	
 	#### defrag
 	
-	###
-	
-	defrag = os.path.join(args.plugin, 'wisecondor/defrag.py')
+	defrag = os.path.join(args.pluginDir, 'wisecondor/defrag.py')
 	male = os.path.join(args.pluginDir, 'data/male-defrag')
 	female = os.path.join(args.pluginDir, 'data/female-defrag')
 	sf = 0.688334125062
@@ -77,11 +88,14 @@ def main(args):
 	
 	###
 	
-	defrag_cmd = 'python {defrag} {male} {female} {test} --scalingFactor {sf} --percYonMales {percYmales} {graph}'.format(defrag=defrag, male=male, female=female , test=args.outDir, sf=sf, percYmales=percYonMales, graph="defrag")
+	print('Defrag analysis...')
+	defrag_cmd = 'python {defrag} {male} {female} {test} --scalingFactor {sf} --percYonMales {percYmales} {graph} > defrag.res'.format(defrag=defrag, male=male, female=female , test=args.outDir, sf=sf, percYmales=percYonMales, graph="defrag")
 	p = subprocess.Popen(defrag_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 	stdout, stderr = p.communicate()
 	if p.returncode != 0:
 		raise Exception(stderr)
+	
+	print('Done')
 
 def wisecondor(bam, input, output, plugin):
 	
@@ -94,7 +108,6 @@ def wisecondor(bam, input, output, plugin):
 	reftable =  os.path.join(plugin, 'data/reftable')
 	
 	### path + output names
-	
 	name = os.path.basename(bam).split('.')[0]
 	bam = os.path.join(input, bam) # -> crush bam into new bam var, full path
 	
@@ -104,7 +117,6 @@ def wisecondor(bam, input, output, plugin):
 	pdf = os.path.join(output, name)
 		
 	### wisecondor analysis
-	
 	pickle_cmd = "samtools view {bam} -q 1 | python {consampy} -outfile {pickle}".format(bam = bam, consampy = consampy, pickle = pickle)
 	jobLauncher(pickle_cmd)
 	
@@ -153,15 +165,15 @@ def getProfiles(readstart, plugin, input, output):
 	###
 	
 	if strand == 'fwd':
-		cmd_0 = "python {getProfile} {nuclTrak}/nuclTrack.{chr} {input}/{name}.{chr}.start.fwd 0 {output}/{name}.{chr}.fwd".format(getProfile=getProfile, nuclTrak=nuclTrak, chr=chr, name=name, input=input, output=output)
-		cmd_1 = "python {getProfile} {nuclTrak}/nuclTrack.{chr} {input}/{name}.{chr}.start.fwd 1 {output}/{name}.{chr}.ifwd".format(getProfile=getProfile, nuclTrak=nuclTrak, chr=chr, name=name, input=input, output=output)
+		cmd_0 = "python {getProfile} {nuclTrack}/nuclTrack.{chr} {input}/{name}.{chr}.start.fwd 0 {output}/{name}.{chr}.fwd".format(getProfile=getProfile, nuclTrack=nuclTrack, chr=chr, name=name, input=input, output=output)
+		cmd_1 = "python {getProfile} {nuclTrack}/nuclTrack.{chr} {input}/{name}.{chr}.start.fwd 1 {output}/{name}.{chr}.ifwd".format(getProfile=getProfile, nuclTrack=nuclTrack, chr=chr, name=name, input=input, output=output)
 		
 		jobLauncher(cmd_0)
 		jobLauncher(cmd_1)
 		
 	elif strand == 'rev':
-		cmd_0 = "python {getProfile} {nuclTrak}/nuclTrack.{chr} {input}/{name}.{chr}.start.rev 1 {output}/{name}.{chr}.rev".format(getProfile=getProfile, nuclTrak=nuclTrak, chr=chr, name=name, input=input, output=output)
-		cmd_1 = "python {getProfile} {nuclTrak}/nuclTrack.{chr} {input}/{name}.{chr}.start.rev 0 {output}/{name}.{chr}.irev".format(getProfile=getProfile, nuclTrak=nuclTrak, chr=chr, name=name, input=input, output=output)
+		cmd_0 = "python {getProfile} {nuclTrack}/nuclTrack.{chr} {input}/{name}.{chr}.start.rev 1 {output}/{name}.{chr}.rev".format(getProfile=getProfile, nuclTrack=nuclTrack, chr=chr, name=name, input=input, output=output)
+		cmd_1 = "python {getProfile} {nuclTrack}/nuclTrack.{chr} {input}/{name}.{chr}.start.rev 0 {output}/{name}.{chr}.irev".format(getProfile=getProfile, nuclTrack=nuclTrack, chr=chr, name=name, input=input, output=output)
 		
 		jobLauncher(cmd_0)
 		jobLauncher(cmd_1)
